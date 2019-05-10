cea2aafc3ad03c3d4fe73c9cf7f269c2236e9180
LiYun <616852971@qq.com>
2019-05-10 18:03:28 +0800

    5.10

--- project/src/components/Buttom.vue
+++ project/src/components/Buttom.vue
@@ -1,11 +1,33 @@
 <template>
-    <div>
-        
-    </div>
+  <component>
+    <span :class="btnStyles">
+      <slot></slot>
+    </span>
+  </component>
 </template>
 
 <script>
+const myComponentsName = "ma_buttom";
 export default {
-    
-}
+  props: {
+    type: {
+      type: String,
+      default: "default"
+    },
+    ghost:{
+        type:Boolean,
+        default:false
+    }
+  },
+  data() {
+    return {
+      myComponentsName: myComponentsName,
+    };
+  },
+  computed:{
+      btnStyles(){
+          
+      }
+  }
+};
 </script>
