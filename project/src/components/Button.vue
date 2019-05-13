dae337ffa03968e27f8c5ef9a18718fd0b0b32df
LiYun <616852971@qq.com>
2019-05-13 10:39:33 +0800

    按钮

---
+++ project/src/components/Button.vue
@@ -0,0 +1,46 @@
+<template>
+    <span :class="btnStyles" class="ma_button_common" @click="childBtnClick">
+      <slot></slot>
+    </span>
+</template>
+
+<script>
+const myComponentsName = "ma_button";
+export default {
+  name:'MaButton',
+  props: {
+    type: {
+      type: String,
+      default: "default"
+    },
+    ghost:{
+        type:Boolean,
+        default:false
+    }
+  },
+  data() {
+    return {
+      myComponentsName: myComponentsName,
+    };
+  },
+  computed:{
+      btnStyles(){
+          switch(this.type){
+            case 'default':
+            return `${myComponentsName}_default`;
+            case 'warn':
+            return `${myComponentsName}_warn`;
+            case 'sure':
+            return `${myComponentsName}_sure`;
+            case 'cancle':
+            return `${myComponentsName}_cancle`
+          }
+      }
+  },
+  methods:{
+    childBtnClick(){
+      this.$emit('click')
+    },
+  }
+};
+</script>
--- project/src/views/About.vue
+++ project/src/views/About.vue
@@ -1,16 +1,27 @@
 <template>
   <div class="about">
     <MaCalendar></MaCalendar>
+    <MaButton>默认按钮</MaButton>
+    <MaButton type="warn" @click="testBtn">警告按钮</MaButton>
+    <MaButton type="sure">确认按钮</MaButton>
+    <MaButton type="cancle">取消按钮</MaButton>
   </div>
 </template>
 <script>
-import MaCalendar from "@/components/Calendar.vue"
+import MaCalendar from "@/components/Calendar.vue";
+import MaButton from "@/components/Button.vue";
 export default {
-  data(){
-    return {}
+  data() {
+    return {};
   },
-  components:{
-    MaCalendar
+  components: {
+    MaCalendar,
+    MaButton
+  },
+  methods:{
+    testBtn(){
+      console.log(111);
+    }
   }
-}
+};
 </script>
